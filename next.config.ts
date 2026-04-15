b957885e40f8c5a2e091d6468cdb6a34fb0ac384
barca <barca@example.com>
2026-04-15 11:25:35 -0600

    fix: next.config.ts serverActions limit fix for Production

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true }
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb'
+    }
+  }
 };
 
 export default nextConfig;
