64d19e9058a75098d6c5eff63cff953bcb1f17c4
barca <barca@example.com>
2026-05-12 13:05:33 -0600

    fix(whatsapp): add webVersionCache to fix QR linking error

--- whatsapp-service/index.js
+++ whatsapp-service/index.js
@@ -18,6 +18,10 @@ const client = new Client({
     puppeteer: {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
         headless: true,
+    },
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
     }
 });
 
