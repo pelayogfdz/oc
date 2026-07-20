2507d37acd4af21c65d31e35bebcae4e4f2653fa
barca <barca@example.com>
2026-07-20 09:38:39 -0600

    feat: bump build version to v2 to propagate search fix

--- app/components/SWCleaner.tsx
+++ app/components/SWCleaner.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 
-const CURRENT_BUILD_VERSION = '2026-07-20-v1';
+const CURRENT_BUILD_VERSION = '2026-07-20-v2';
 
 export default function SWCleaner() {
   useEffect(() => {
