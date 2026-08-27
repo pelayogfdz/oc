9c41d12680c86030b7a6075243cb76b3150e87d5
barca <barca@example.com>
2026-08-27 15:52:51 -0600

    chore: update push_schema_hetzner helper script

--- push_schema_hetzner.js
+++ push_schema_hetzner.js
@@ -2,6 +2,9 @@ const { Client } = require('ssh2');
 const fs = require('fs');
 const path = require('path');
 
+const localSchema = fs.readFileSync(path.join(__dirname, 'prisma/schema.prisma'), 'utf8');
+const schemaB64 = Buffer.from(localSchema).toString('base64');
+
 const scriptStr = `
 const { execSync } = require('child_process');
 const dbs = [
@@ -15,7 +18,7 @@ for (const db of dbs) {
   const url = \`postgresql://postgres:caanma_postgres_secure_2026@db:5432/\${db}?sslmode=disable\`;
   console.log(\`Pushing schema to \${db}...\`);
   try {
-    const output = execSync(\`npx prisma db push --skip-generate\`, {
+    const output = execSync(\`npx prisma db push --schema=/tmp/schema.prisma --skip-generate\`, {
       env: { ...process.env, DATABASE_URL: url },
       stdio: 'inherit'
     });
@@ -28,7 +31,8 @@ for (const db of dbs) {
 const conn = new Client();
 conn.on('ready', () => {
   const scriptB64 = Buffer.from(scriptStr).toString('base64');
-  conn.exec(`docker exec -i caanma-app sh -c "echo '${scriptB64}' | base64 -d > /tmp/push_schema.js && node /tmp/push_schema.js"`, (err, stream) => {
+  // First upload the new schema to /tmp/schema.prisma, then run the push script
+  conn.exec(`docker exec -i caanma-app sh -c "echo '${schemaB64}' | base64 -d > /tmp/schema.prisma && echo '${scriptB64}' | base64 -d > /tmp/push_schema.js && node /tmp/push_schema.js"`, (err, stream) => {
     if (err) throw err;
     stream.on('data', d => process.stdout.write(d));
     stream.stderr.on('data', d => process.stderr.write(d));
